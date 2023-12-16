af02506d7ac9dcb05763a389bad607ec9f60bead
toLight <tolightbook@gmail.com>
2023-12-16 19:20:07 +0200

    Initial Commit

---
+++ InteractiveChart/InteractiveChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveChartApp.swift
+//  InteractiveChart
+//
+//  Created by Denys Nazymok on 16.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
